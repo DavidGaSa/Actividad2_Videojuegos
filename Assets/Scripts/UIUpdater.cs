ad351d2c6b5a75b985408362deb454979f6854bb
David Garcia <garciasalgadodavid4@gmail.com>
2026-03-12 18:25:13 +0100

    Escenas de bucle de juegos y arrelgos prefabs

--- Assets/Scripts/UIUpdater.cs
+++ Assets/Scripts/UIUpdater.cs
@@ -5,11 +5,11 @@ using System.Runtime.CompilerServices;
 
 public class UIUpdater : MonoBehaviour
 {
-
+    //Creamos las variables que manejan el texto.
     [SerializeField] private TextMeshProUGUI textoMonedas;
     [SerializeField] private TextMeshProUGUI textoVidas;
 
-
+    //Creamos los métodos que actualizan el texto de las monedas y las vidas.
     private void actualizarMonedas(int coins)
     {
         textoMonedas.text = "Monedas: " + coins.ToString();
@@ -20,16 +20,13 @@ public class UIUpdater : MonoBehaviour
         textoVidas.text = "Vidas: " + vidas.ToString();
     }
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    //En el Start, nos suscribimos a los eventos de cambio de monedas y vidas para actualizar el texto cada vez que cambien.
     void Start()
-
     {
         actualizarMonedas(GameManager.Instance.Coins); // Actualiza el texto al iniciar
         actualizarVidas(GameManager.Instance.Vidas); // Actualiza el texto al iniciar
         GameManager.Instance.OnCoinsChanged += actualizarMonedas; //Indica el contador cada vez que cambie el número de monedas
         GameManager.Instance.OnVidasChanged += actualizarVidas; //Indica el contador cada vez que cambie el número de vidas
-
-        
     }
 
     private void OnDestroy()
